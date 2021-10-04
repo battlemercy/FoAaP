3a33a034c8b9a9c179c10d91cfe8562d1c54571d
battlemercy <79182400+battlemercy@users.noreply.github.com>
2021-10-04 14:55:21 +0700

    Add files via upload

---
+++ Лаба 7/UnitTest/UnitTest.cpp
@@ -0,0 +1,51 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Лаба 7/splitter.cpp"
+#include <algorithm>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestSplit)
+		{
+			std::string str{"25 0 18 128 269"};
+			auto result{split(str)};
+
+			std::vector<unsigned int> numbers{25, 0, 18, 128, 269};
+
+			auto mismatch{ std::mismatch(result.cbegin(), result.cend(), numbers.cbegin()) };
+			if (mismatch.first == result.cend() && mismatch.second == numbers.cend())
+			{
+				EXIT_SUCCESS;
+			}
+			else
+			{
+				EXIT_FAILURE;
+			}
+		}
+
+
+		TEST_METHOD(TestRemoveOdds)
+		{
+			std::vector<unsigned int> numbers{ 25, 0, 18, 128, 269 };
+			auto odds{removeOdds(numbers)};
+
+			std::vector<unsigned int> result{ 25, 269 };
+
+			auto mismatch{ std::mismatch(result.cbegin(), result.cend(), numbers.cbegin()) };
+			if (mismatch.first == result.cend() && mismatch.second == numbers.cend())
+			{
+				EXIT_SUCCESS;
+			}
+			else
+			{
+				EXIT_FAILURE;
+			}
+		}
+	};
+}
---
+++ Лаба 7/Лаба 7/splitter.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+std::vector<unsigned int> split(const std::string& str);
+
+std::vector<unsigned int> removeOdds(const std::vector<unsigned int>& numbers);
