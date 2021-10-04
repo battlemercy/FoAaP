6dc093c7d7e0a9855799a30bca9aba4d0d3c1a17
battlemercy <79182400+battlemercy@users.noreply.github.com>
2021-10-04 14:57:07 +0700

    Delete Лаба 7 directory

--- Лаба 7/Лаба 7/splitter.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <string>
-
-std::vector<unsigned int> split(const std::string& str);
-
-std::vector<unsigned int> removeOdds(const std::vector<unsigned int>& numbers);
